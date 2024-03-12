66a9def1e5077bf842e26cfc6b2a19ddb357244c
Minh Au <auduongminh2004@gmail.com>
2024-03-11 23:48:31 -0400

    capture tty from current running process

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,7 +1,24 @@
 use crate::editor;
-use std::io::{self, Write};
+use std::process::{Command, Stdio};
 
 pub fn start_editor() {
     editor::start_editor();
-    let _ = io::stdout().write_all(b"Starting the editor directly");
+    match catpure_tty() {
+        Ok(v) => print!("{}", v),
+        Err(_e) => print!("Cannot start the editor"),
+    }
+}
+
+fn catpure_tty() -> Result<String, String>{
+    let proc = Command::new("tty")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute process");
+    let output = proc
+        .wait_with_output()
+        .expect("Cannot wait for the process to finish executing");
+    if output.status.success() {
+        return Ok(String::from_utf8(output.stdout).expect("Cannot convert tty ouput to string"));
+    }
+    Err(String::from("Cannot obtain TTY"))
 }
