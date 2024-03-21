7b46fc5ee53e24b16201f44f2bfa4331f959bb4e
Minh Au <auduongminh2004@gmail.com>
2024-03-20 23:04:37 -0400

    convert the terminal into using the controllers enums

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,4 @@
-use crate::editor;
+use crate::editor::*;
 use std::fs::File;
 use std::os::fd::AsRawFd;
 use std::process::{Command, Stdio};
@@ -6,11 +6,12 @@ use std::io::{self, Read, Write};
 use termios;
 
 pub fn start_editor() -> Result<(), io::Error>{
-    editor::start_editor();
+    let mut editor = Editor::new();
+
     let tty          = catpure_tty()?;
     let tty          = &tty[..tty.len()-1]; // remove ending new-line
 
-    let mut tty_file = File::open(tty)?;
+    let tty_file = File::open(tty)?;
     let raw_fd       = tty_file.as_raw_fd();
     let old_term     = termios::Termios::from_fd(raw_fd).expect("Cannot access terminal raw fd");
     let mut term     = old_term.clone();
@@ -23,28 +24,41 @@ pub fn start_editor() -> Result<(), io::Error>{
     let _ = io::stdout().lock();
      
     let mut curr_byte = tty_file.bytes();
-    let mut input     = b'0';
+    // let mut input     = b'0';
 
-    while b'q' != input {
-        input = match curr_byte.next() {
-            Some(v) => v.unwrap(),
-            None    => 0,
-        };
+    loop {
+        match editor.get_state() {
+            State::IN_SESSION => {
+                let input = match curr_byte.next() {
+                    Some(v) => v.unwrap(),
+                    None    => 0,
+                };
 
-        let _ = parse_input(input);
+                let event = match parse_input(input) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        let _ = termios::tcsetattr(raw_fd, termios::TCSAFLUSH, &old_term);
+                        return Err(e);
+                    }
+                };
+
+                editor.process_event(event);
+            },
+            _ => {
+                let _ = termios::tcsetattr(raw_fd, termios::TCSAFLUSH, &old_term);
+                break;
+            }
+        }
     }
-
-    let _ = termios::tcsetattr(raw_fd, termios::TCSAFLUSH, &old_term);
 
     Ok(())
 }
 
-fn parse_input(input: u8) -> io::Result<()>{
-    let _ = match input {
-        b'\x1b' => io::stdout().write_all("ESC".as_bytes()),
-        _       => io::stdout().write_all(&vec![input]),
-    };
-    io::stdout().flush()
+fn parse_input(input: u8) -> io::Result<Event>{
+    match input {
+        b'\x1b' => Ok(Event::ESCAPE),
+        _       => Ok(Event::ASCII(input as char)), // TODO: is_ascii()
+    }
 }
 
 fn catpure_tty() -> Result<String, io::Error>{
