064d9ce0f44814175c70cdfe01a7a7dc7747eb05
Minh Au <auduongminh2004@gmail.com>
2024-03-20 23:04:16 -0400

    add enums for EVENT/STATE/MODE and editor struct for processing them

--- src/editor.rs
+++ src/editor.rs
@@ -1,3 +1,61 @@
-pub fn start_editor() {
-    // println!("Starting Editor in the background");
+use std::io::{self, Write};
+
+pub enum Event {
+    ESCAPE,
+    CTRL,
+    SHIFT,
+    OPTION,
+    CMD,
+    ASCII(char),
+}
+
+pub enum Mode {
+    EDIT,
+    VISUAL,
+    COMMAND,
+}
+
+#[derive(Copy, Clone)]
+pub enum State {
+    START,
+    IN_SESSION,
+    EXIT
+}
+
+pub struct Editor {
+    state: State,
+    buffer: Option<Event>, // TODO: should have internal buffer for something
+                    // Right now, it's just storing the last event
+}
+
+impl Editor {    
+    pub fn new() -> Self {
+        let state = State::IN_SESSION;
+        Editor {
+            state,
+            buffer: None
+        }
+    }
+
+    pub fn get_state(&self) -> State {
+        self.state 
+    }
+
+    pub fn process_event(&mut self, event: Event) { // TODO: -> Result<...> instead
+        match event {
+            Event::ASCII(c) => {
+                if 'q' == c {
+                    self.state = State::EXIT;
+                }
+                print!("{}", c); // TODO: remove these calls. Should not have no frontend
+                io::stdout().flush();
+            }
+            _ => {
+                // TODO: do something about control characters
+            }
+        }
+        self.buffer = Some(event);
+    }
+
+    // TODO: print/render, allow a arbitrary front-end trait to be passed in 
 }
